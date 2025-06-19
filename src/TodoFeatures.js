ccbe9bd1835b6559e67a40825679fe93cd4fdfc8
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-06-19 22:50:02 +0800

    .

--- src/TodoFeatures.js
+++
@@ -1,3 +0,0 @@
-export default function ToDo_Features(title, description, dueDate, priority){
-    return {title, description, dueDate, priority}
-}
---
+++ src/createTodo.js
@@ -0,0 +1,3 @@
+export default function Todo(title, description, dueDate, priority){
+    return {title, description, dueDate, priority}
+}
